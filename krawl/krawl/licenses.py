1256ca96bd17a97799c3e7d92b4d209bc1f43642
hoijui <hoijui.quaero@gmail.com>
2021-11-09 09:40:08 +0100

    Adds a FIXME comment

--- krawl/krawl/licenses.py
+++ krawl/krawl/licenses.py
@@ -20,6 +20,16 @@ def getlicenses():
 @lru_cache()
 def getlicenseblacklists():
     res = requests.get(
+        # NOTE TODO FIXME This list was available under the original URL:
+        # https://raw.githubusercontent.com/OPEN-NEXT/LOSH/master/Data%20Mapping/SPDX-blacklist"
+        # in one-column CSV format (which is what this code here assumes),
+        # but has since moved to
+        # https://raw.githubusercontent.com/OPEN-NEXT/LOSH-Licenses/main/SPDX-blocklist.json
+        # and been converted to JSON.
+        # Thus, this code is broken at the moment.
+        # My (hoijui) goal is, to get it available as CSV again;
+        # see issue:
+        # https://github.com/OPEN-NEXT/LOSH-Licenses/issues/1#issuecomment-963914298
         "https://raw.githubusercontent.com/OPEN-NEXT/LOSH/master/Data%20Mapping/SPDX-blacklist"
     )
     elems = res.text.strip().split("\n")
