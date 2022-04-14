59184aacd0fdfafc758cbba66399df2ddec48c39
Robin Vobruba <hoijui.quaero@gmail.com>
2022-04-14 14:09:20 +0200

    Reads upload-method in manifests, if available

--- krawl/normalizer/manifest.py
+++ krawl/normalizer/manifest.py
@@ -61,7 +61,7 @@ class ManifestNormalizer(Normalizer):
         project.user_manual = self._file(raw.get("user-manual"), project.meta.path, download_url)
         project.part = self._parts(raw.get("part"), project.meta.path, download_url)
         project.software = self._software(raw.get("software"), project.meta.path, download_url)
-        project.upload_method = UploadMethods.MANIFEST
+        project.upload_method = raw.get("upload-method", UploadMethods.MANIFEST)
 
         return project
 
