9036ee0886c9bf4cc92dddf6635872673b75ad76
Nicolas Traeder <nicolas@konek.to>
2022-04-21 11:31:23 +0200

    Rewrite some wikibase code to support utf8 chars in name of the project

--- krawl/wikibase/core.py
+++ krawl/wikibase/core.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import rdflib
 import rdflib as r
 from rdflib import RDF, RDFS, Graph
 
@@ -22,22 +22,26 @@ def makeentity(reconcile_property, subject, g, valuereps=None):
     if valuereps is None:
         valuereps = {}
     entity = {"label": None}
-    base = dict(g.namespaces())['']
+
     statements = [{"property": reconcile_property, "value": str(subject)}]
     predicates = g.predicate_objects(subject)
-    for i, pred in enumerate(predicates):
-        print(pred)
-        a, v = pred
+    namespaces_dict = dict(g.namespaces())
+    predicates_dict = dict(predicates)
+
+    if '' in namespaces_dict:
+        base = namespaces_dict['']
+    else:
+        base = predicates_dict[
+                   rdflib.term.URIRef(OKH + 'repo')] + "/" + \
+               predicates_dict[
+                   rdflib.term.URIRef(OKH + 'version')] + "/"
+
+    for a in predicates_dict:
+        v = predicates_dict[a]
         statement = None
-        if pred:
-            print("PRED: ", pred)
         if a == RDFS.label:
-            if pred:
-                print(f"{i} Label found", a == RDFS.label, v)
             entity["label"] = v
         elif OKH in a:
-            if pred:
-                print("  OKH in a")
             prop = a.replace(OKH, "")
             statement = {
                 "property": prop,
@@ -45,21 +49,15 @@ def makeentity(reconcile_property, subject, g, valuereps=None):
                 "_datatype": DATATYPES.get(prop, "wikibase-item"),
             }
             if isinstance(statement["value"], r.term.URIRef):
-                if pred:
-                    print("   url")
                 if base in statement["value"]:
                     # we got a sub item.. and keep the wikibase-item datatype
                     pass
                 else:
                     statement["_datatype"] = "url"
             if isinstance(statement["value"], r.term.Literal):
-                if pred:
-                    print("   literal")
                 statement["_datatype"] = "string"
             statements.append(statement)
         elif str(RDF) in a:
-            if pred:
-                print("  RDF in a")
             prop = a.replace(str(RDF), "")
             statement = {
                 "property": prop,
@@ -67,21 +65,14 @@ def makeentity(reconcile_property, subject, g, valuereps=None):
                 "_datatype": DATATYPES.get(prop, "wikibase-item"),
             }
             if isinstance(statement["value"], r.term.URIRef):
-                if pred:
-                    print("   url")
                 if base in statement["value"]:
                     # we got a sub item.. and keep the wikibase-item datatype
                     pass
                 else:
                     statement["_datatype"] = "url"
             if isinstance(statement["value"], r.term.Literal):
-                if pred:
-                    print("   literal")
                 statement["_datatype"] = "text"
             statements.append(statement)
-        else:
-            if pred:
-                print("   else", a)
     entity["statements"] = statements
     return entity
 
