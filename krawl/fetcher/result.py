3d2ab993dfccc2257c2afce9639ceb86aaaed355
Robin Vobruba <hoijui.quaero@gmail.com>
2024-12-27 20:26:15 +0100

    Undo trickery against circular imports
    Yuhuu, not needed anymore!

--- krawl/fetcher/result.py
+++ krawl/fetcher/result.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TypeAlias
 
-# NOTE We do this strange typing hackery to avoid circular imports
-_DataSet: TypeAlias = object
-_Manifest: TypeAlias = object
+from krawl.model.data_set import DataSet
+from krawl.model.manifest import Manifest
 
 
 @dataclass(slots=True, frozen=True)
@@ -14,13 +12,7 @@ class FetchResult:
     This might be OKH data, or projects hosting systems native format
     (e.g. whatever the Thingiverse API returns about a project;
     probably in JSON format)."""
-    data_set: _DataSet = None  # Meta-data about the crawl
-    data: _Manifest = None  # The actually main content of the crawl; yummy, yummy data!
-
-
-# pylint: disable=wrong-import-position
-from krawl.model.data_set import DataSet
-from krawl.model.manifest import Manifest
-
-_DataSet: TypeAlias = DataSet
-_Manifest: TypeAlias = Manifest
+    data_set: DataSet
+    """Meta-data about the crawl"""
+    data: Manifest
+    """The actually main content of the crawl; yummy, yummy data!"""
