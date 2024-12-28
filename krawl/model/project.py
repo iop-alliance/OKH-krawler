18ee6d3e82f38a79ef009fd481500d1f91e5d051
Robin Vobruba <hoijui.quaero@gmail.com>
2024-12-28 08:51:19 +0100

    Overhaul Project [fix]

--- krawl/model/project.py
+++ krawl/model/project.py
@@ -6,12 +6,12 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 
+from krawl.model.agent import Agent
 from krawl.model.file import File
 from krawl.model.licenses import License
 from krawl.model.part import Part
 from krawl.model.project_id import ProjectId
 from krawl.model.software import Software
-from krawl.model.sourcing_procedure import SourcingProcedure
 
 
 @dataclass(slots=True)
@@ -20,32 +20,31 @@ class Project:  # pylint: disable=too-many-instance-attributes
     <https://github.com/iop-alliance/OpenKnowHow/blob/master/res/sample_data/okh-TEMPLATE.toml>. # TODO should be based on okh.ttl instead
     """
 
-    okhv: str = "OKH-LOSHv1.0"
-    name: str = None
-    repo: str = None
-    version: str = None
-    release: str = None
-    license: License = None
-    licensor: str = None
-    organization: str = None
-    readme: File = None
-    contribution_guide: File = None
+    # okhv: str = "OKH-LOSHv1.0"
+    name: str
+    repo: str
+    version: str
+    license: License
+    release: str | None = None
+    licensor: list[Agent] = field(default_factory=list)
+    organization: str | None = None
+    readme: File | None = None
+    contribution_guide: File | None = None
     image: list[File] = field(default_factory=list)
-    documentation_language: str = None
-    technology_readiness_level: str = None
-    documentation_readiness_level: str = None
-    attestation: str = None
-    publication: str = None
-    function: str = None
-    standard_compliance: str = None
-    cpc_patent_class: str = None
-    tsdc: str = None
-    bom: File = None
-    manufacturing_instructions: File = None
-    user_manual: File = None
+    documentation_language: str | None = None
+    technology_readiness_level: str | None = None
+    documentation_readiness_level: str | None = None
+    attestation: list[str] = field(default_factory=list)
+    publication: list[str] = field(default_factory=list)
+    function: str | None = None
+    standard_compliance: list[str] = field(default_factory=list)
+    cpc_patent_class: str | None = None
+    tsdc: str | None = None
+    bom: File | None = None
+    manufacturing_instructions: File | None = None
+    user_manual: File | None = None
     part: list[Part] = field(default_factory=list)
     software: list[Software] = field(default_factory=list)
-    sourcing_procedure: SourcingProcedure = None
     source: list[File] = field(default_factory=list)
     export: list[File] = field(default_factory=list)
 
