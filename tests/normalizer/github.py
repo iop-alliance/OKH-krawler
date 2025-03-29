82249f8684f5bc80a06f7a2ef639487f1036539f
Robin Vobruba <hoijui.quaero@gmail.com>
2025-03-29 09:40:19 +0100

    Format test sources

--- tests/normalizer/github.py
+++ tests/normalizer/github.py
@@ -11,15 +11,16 @@ from krawl.normalizer.github import GitHubFileHandler
 
 log = get_child_logger("github")
 
-BASE_URL='https://github.com'
-SLUG='iop-alliance/OpenKnowHow'
-FILE_RELATIVE_PATH='.gitignore'
-DEV_BRANCH='master'
-VERSION='v1.1.0'
-FILE_WEB_URL=BASE_URL + '/' + SLUG + '/blob/master/.gitignore'
-FILE_DL_URL=BASE_URL + '/' + SLUG + '/raw/master/.gitignore'
-FILE_FROZEN_WEB_URL=BASE_URL + '/' + SLUG + '/blob/' + VERSION + '/.gitignore'
-FILE_FROZEN_DL_URL=BASE_URL + '/' + SLUG + '/raw/' + VERSION + '/.gitignore'
+BASE_URL = 'https://github.com'
+SLUG = 'iop-alliance/OpenKnowHow'
+FILE_RELATIVE_PATH = '.gitignore'
+DEV_BRANCH = 'master'
+VERSION = 'v1.1.0'
+FILE_WEB_URL = BASE_URL + '/' + SLUG + '/blob/master/.gitignore'
+FILE_DL_URL = BASE_URL + '/' + SLUG + '/raw/master/.gitignore'
+FILE_FROZEN_WEB_URL = BASE_URL + '/' + SLUG + '/blob/' + VERSION + '/.gitignore'
+FILE_FROZEN_DL_URL = BASE_URL + '/' + SLUG + '/raw/' + VERSION + '/.gitignore'
+
 
 class TestStringMethods(unittest.TestCase):
 
@@ -45,6 +46,7 @@ class TestStringMethods(unittest.TestCase):
         file_handler = GitHubFileHandler()
         proj_info = file_handler.gen_proj_info_raw(SLUG, VERSION, DEV_BRANCH)
         self.assertEqual(file_handler._extract_slug(FILE_FROZEN_DL_URL), SLUG)
+
 
 if __name__ == '__main__':
     unittest.main()
